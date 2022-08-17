c754269393088c18540b60a1da27aa4436b115a0
G.S.I.S <webmaster@agency.co.il>
2022-08-17 16:52:06 +0300

    another syntax

--- javascripts/discourse/components/custom-tiles.js
+++ javascripts/discourse/components/custom-tiles.js
@@ -63,8 +63,8 @@ export default Component.extend({
     const titleImg2 = settings.tile_2_image;
     const titleImg3 = settings.tile_3_image;
     const titleImg4 = settings.tile_4_image;
-    
-    this.$('#tile-img-1').src = titleImg1;
+
+    this.$('#tile-img-1').attr({'src': titleImg1});
   },
 
   didDestroyElement() {
