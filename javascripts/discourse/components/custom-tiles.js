8484a7fe1b60f734e919d91722640d36e337bb8e
G.S.I.S <webmaster@agency.co.il>
2022-08-17 17:03:53 +0300

    differently

--- javascripts/discourse/components/custom-tiles.js
+++ javascripts/discourse/components/custom-tiles.js
@@ -51,15 +51,16 @@ export default Component.extend({
     );
   },
 
-  didInsertElement() {
-    this.displayChanged();    
-
+  didInsertElement() {      
+    this._super(...arguments);
     const titleImg1 = settings.tile_1_image;
     const titleImg2 = settings.tile_2_image;
     const titleImg3 = settings.tile_3_image;
     const titleImg4 = settings.tile_4_image;
-    const img1 = this.$('#tile-img-1');
-    img1.attr('src',titleImg1);
+    
+    this.$('#tile-img-1').attr('src',titleImg1);
+
+    this.displayChanged();
 
   },
 
