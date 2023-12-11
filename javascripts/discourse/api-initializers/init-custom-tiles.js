96ac492f76b840f6c60fa178b8ba2fcfc6be7184
gal-samuel-israel <webmaster@agency.co.il>
2023-12-11 12:44:08 +0200

    registerConnectorClass was deprecated

--- javascripts/discourse/api-initializers/init-custom-tiles.js
+++ javascripts/discourse/api-initializers/init-custom-tiles.js
@@ -1,5 +1,6 @@
 import { apiInitializer } from "discourse/lib/api";
 
+/* Deprecated
 export default apiInitializer("0.8", (api) => {
   const enableConnectorName = settings.plugin_outlet;
   const disableConnectorName =
@@ -17,4 +18,24 @@ export default apiInitializer("0.8", (api) => {
     tagName: "div.custom-tiles-widget",
   });
 
+});
+*/
+export default apiInitializer("0.8", (api) => {
+  const enableConnectorName = settings.plugin_outlet;
+  const disableConnectorName =
+    enableConnectorName === "above-main-container"
+      ? "below-site-header"
+      : "above-main-container";
+
+  api.decorateWidget("above-site-header", (helper) => {
+    return helper.attach("custom-tiles-widget");
+  });
+
+  api.decorateWidget("above-main-container", (helper) => {
+    return helper.attach("custom-tiles-widget");
+  });
+
+  api.createWidget("custom-tiles-widget", {
+    tagName: "div.custom-tiles-widget",
+  });
 });
