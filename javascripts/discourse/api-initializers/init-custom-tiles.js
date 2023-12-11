0d96b6bd3c5e8396aa6d78d1404c4c3898fd61bc
gal-samuel-israel <webmaster@agency.co.il>
2023-12-11 13:04:35 +0200

    try hide the deprecated method

--- javascripts/discourse/api-initializers/init-custom-tiles.js
+++ javascripts/discourse/api-initializers/init-custom-tiles.js
@@ -1,41 +1,22 @@
 import { apiInitializer } from "discourse/lib/api";
 
-/* Deprecated
 export default apiInitializer("0.8", (api) => {
-  const enableConnectorName = settings.plugin_outlet;
-  const disableConnectorName =
-    enableConnectorName === "above-main-container"
+  const settingsOutlet = settings.plugin_outlet;
+  const finalOutlet =
+  settingsOutlet === "above-main-container"
       ? "below-site-header"
       : "above-main-container";
 
-  api.registerConnectorClass(disableConnectorName, "custom-tiles", {
+  /* DEPRECATED
+  api.registerConnectorClass(finalOutlet, "custom-tiles", {
     shouldRender() {
       return false;
     },
   });
-
+  */
+ 
   api.createWidget("custom-tiles-widget", {
     tagName: "div.custom-tiles-widget",
   });
 
-});
-*/
-export default apiInitializer("0.8", (api) => {
-  const enableConnectorName = settings.plugin_outlet;
-  const disableConnectorName =
-    enableConnectorName === "above-main-container"
-      ? "below-site-header"
-      : "above-main-container";
-
-  api.decorateWidget("above-site-header", (helper) => {
-    return helper.attach("custom-tiles-widget");
-  });
-
-  api.decorateWidget("above-main-container", (helper) => {
-    return helper.attach("custom-tiles-widget");
-  });
-
-  api.createWidget("custom-tiles-widget", {
-    tagName: "div.custom-tiles-widget",
-  });
 });
