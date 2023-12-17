9c65e5df4ac8eacc74b96564baec9227571ecefb
gal-samuel-israel <webmaster@agency.co.il>
2023-12-17 16:24:43 +0200

    finalize

--- javascripts/discourse/api-initializers/init-custom-tiles.js
+++ javascripts/discourse/api-initializers/init-custom-tiles.js
@@ -4,20 +4,25 @@ import customTilesComponent from "../components/custom-tiles";
 export default apiInitializer("0.8", (api) => {
   const settingsOutlet = settings.plugin_outlet;
   const selectedOutlet = settingsOutlet === "above-main-container" ? "above-main-container" : "below-site-header";
-
-/* Solution was to remove the connector from below-site-header so that the component will not show 2 times */
-/* DEPRECATED but works 
+  api.renderInOutlet(selectedOutlet, customTilesComponent);
+/* 
+api.registerConnectorClass was DEPRECTAED
+231217 - Solution Found with help of Discourse Support.
+the  custom.tiles.hbs file moved into the components directory with its custom.tiles.js related file.
+api.createWidget directive was also disabled - seems that it is not required any more
+the outlet selection was updated into a simple 1 of 2 options
+*/
+/* 
   api.registerConnectorClass(selectedOutlet, "custom-tiles", {
     shouldRender() {
       return false;
     },
   });
-*/
-  /* not deprecated but does not work */
-  api.renderInOutlet(selectedOutlet, customTilesComponent);
-  /*
+
   api.createWidget("custom-tiles-widget", {
     tagName: "div.custom-tiles-widget",
   });
-  */
+
+*/  
+
 });
