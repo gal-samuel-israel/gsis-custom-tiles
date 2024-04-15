078b92a107fdefac3a0e674e8bcd5d421b5743f4
gal-samuel-israel <webmaster@agency.co.il>
2024-04-15 15:07:15 +0300

    add missing parts

--- javascripts/discourse/components/custom-tiles.js
+++ javascripts/discourse/components/custom-tiles.js
@@ -85,19 +85,29 @@ export default Component.extend({
   },
   didRender(){
     this._super(...arguments);
-   const assets = settings.theme_uploads;
+    const assets = settings.theme_uploads;
     //console.log(assets);
 
-    //Uploads or Assets
-    const img1 = (settings.tile_1_image!=='' && settings.tile_1_image!==null)?settings.tile_1_image:assets.img_get_started;
-    const img2 = (settings.tile_2_image!=='' && settings.tile_2_image!==null)?settings.tile_2_image:assets.img_ask_the_community;
-    const img3 = (settings.tile_3_image!=='' && settings.tile_3_image!==null)?settings.tile_3_image:assets.img_announcements;
-    const img4 = (settings.tile_4_image!=='' && settings.tile_4_image!==null)?settings.tile_4_image:assets.img_show_and_tell;
+    const vEnableTilesImages = this.get("enableTilesImages");
+    const vEnableBanner = this.get("enableBanner");
 
-    jQuery('#tile-img-1 img.tile-img-thumb').attr('src',img1);
-    jQuery('#tile-img-2 img.tile-img-thumb').attr('src',img2);
-    jQuery('#tile-img-3 img.tile-img-thumb').attr('src',img3);
-    jQuery('#tile-img-4 img.tile-img-thumb').attr('src',img4);
+    if(vEnableTilesImages){
+      //Uploads or Assets
+      const img1 = (settings.tile_1_image!=='' && settings.tile_1_image!==null)?settings.tile_1_image:assets.img_get_started;
+      const img2 = (settings.tile_2_image!=='' && settings.tile_2_image!==null)?settings.tile_2_image:assets.img_ask_the_community;
+      const img3 = (settings.tile_3_image!=='' && settings.tile_3_image!==null)?settings.tile_3_image:assets.img_announcements;
+      const img4 = (settings.tile_4_image!=='' && settings.tile_4_image!==null)?settings.tile_4_image:assets.img_show_and_tell;
+
+      jQuery('#tile-img-1 img.tile-img-thumb').attr('src',img1);
+      jQuery('#tile-img-2 img.tile-img-thumb').attr('src',img2);
+      jQuery('#tile-img-3 img.tile-img-thumb').attr('src',img3);
+      jQuery('#tile-img-4 img.tile-img-thumb').attr('src',img4);
+    }
+
+    if(vEnableBanner){
+      const imgBanner = (settings.banner_image!=='' && settings.banner_image!==null) ? settings.banner_image:assets.img_show_and_tell;
+      jQuery('#custom-tiles-banner').attr('src', imgBanner);
+    }
     
   },
   didDestroyElement() {
