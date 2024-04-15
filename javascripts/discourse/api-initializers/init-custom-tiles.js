2e70ee55e0e7f35288c8eab5a62de0b03ee7ea8b
gal-samuel-israel <webmaster@agency.co.il>
2024-04-15 12:24:02 +0300

    add debug layer to initializer

--- javascripts/discourse/api-initializers/init-custom-tiles.js
+++ javascripts/discourse/api-initializers/init-custom-tiles.js
@@ -1,29 +1,28 @@
 import { apiInitializer } from "discourse/lib/api";
 import customTilesComponent from "../components/custom-tiles";
 
-export default apiInitializer("0.8", (api) => {
+export default apiInitializer("1.6", (api) => {
+
+
+  if (api.getCurrentUser()) {
+    const currentUser = api.getCurrentUser()
+
+    var debug = currentUser.admin && settings.enable_debug_for_admins;
+    var debugForUsers = settings.enable_debug_for_user_ids;
+    var debugForIDs = (debugForUsers) ? debugForUsers.split("|") : null;
+    if (debugForIDs && debugForIDs.includes(currentUser.id.toString())) { debug = true; }
+
+    var debug4All = settings.enable_debug_for_all;  if(debug4All){ debug = true; } 
+
+    if(debug){          
+      console.log('custom-tiles-row debug is ON');
+      console.log('admin:' + currentUser.admin); 
+      console.log('currentUser.external_id:' + currentUser.external_id); 
+    }
+  }
+
   const settingsOutlet = settings.plugin_outlet;
   const selectedOutlet = settingsOutlet === "above-main-container" ? "above-main-container" : "below-site-header";
   api.renderInOutlet(selectedOutlet, customTilesComponent);
-/* 
-api.registerConnectorClass was DEPRECTAED
-231217 - Solution Found with help of Discourse Support.
-the  custom.tiles.hbs file moved into the components directory with its custom.tiles.js related file.
-api.createWidget directive was also disabled - seems that it is not required any more
-the outlet selection was updated into a simple 1 of 2 options
-old folders for template positioning etc' were removed
-*/
-/* 
-  api.registerConnectorClass(selectedOutlet, "custom-tiles", {
-    shouldRender() {
-      return false;
-    },
-  });
-
-  api.createWidget("custom-tiles-widget", {
-    tagName: "div.custom-tiles-widget",
-  });
-
-*/  
 
 });
